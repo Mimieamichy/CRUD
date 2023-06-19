1aecf32b738c589039a39e47f3166e84a332ce68
Mimieamichy <93717035+Mimieamichy@users.noreply.github.com>
2023-06-19 10:47:04 +0100

    password validation

--- login.js
+++
@@ -1,7 +0,0 @@
-const wrapper = document.querySelector('.wrapper');
-const btnPopUp = document.getElementById('#btn-popUp');
-const iconClose = document.querySelector('.i');
-
-btnPopUp.addEventListener('click', ()=> {
-  wrapper.classList.remove('active-popup');
-})
